e0b89af13080f51c3deea4513c279b5f941d4ee6
Curt Wilhelm <curt_wilhelm@yahoo.com>
2016-02-22 09:30:24 -0700

    update generator to add javascript asset

--- lib/generators/plaid_rails/install_generator.rb
+++ lib/generators/plaid_rails/install_generator.rb
@@ -5,7 +5,12 @@ module PlaidRails
     def install_initializer
       initializer 'plaid.rb', File.read(File.expand_path('../templates/initializer.rb', __FILE__))
     end
-
+    def install_js
+      inject_into_file 'app/assets/javascripts/application.js', after: "//= require jquery\n" do <<-'JS'
+//= require plaid_rails
+        JS
+      end
+    end
     def install_route
       route "mount PlaidRails::Engine => '/plaid', as: :plaid_rails"
     end
